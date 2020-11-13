964974a086aa26927ceda1ecc3c41d85ff57d856
Bart Callant <bart@callant.net>
2020-11-13 08:18:02 +0000

    more config

--- docker.sh
+++ docker.sh
@@ -2,5 +2,5 @@
 
 echo "Started: Setting up docker..."
 docker run -i -d -t -p 8000:8000 --name=dynamodb --restart=unless-stopped amazon/dynamodb-local
-docker run -d -p 9200:9200 -p 5601:5601 --name=elasticsearch --restart=unless-stopped elasticsearch:6.8.6
+docker run -d -p 9200:9200 -p 5601:5601 --name=elasticsearch elasticsearch:6.8.6
 echo "Finished: Setting up docker"
