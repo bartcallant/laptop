d38c2ef5697790f1cc7a0247516c62faab28f65e
Bart Callant <bart@callant.net>
2023-01-27 22:56:37 +0100

    Flatpak updates

--- linux/flatpak.sh
+++ linux/flatpak.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 echo "Started: Installing GUI tools with flatpak..."
+flatpak install flathub com.brave.Browser
+flatpak install flathub com.getpostman.Postman
 flatpak install flathub com.slack.Slack
 flatpak install flathub com.spotify.Client
 flatpak install flathub com.uploadedlobster.peek
 flatpak install flathub com.visualstudio.code
-flatpak install flathub com.uploadedlobster.peek
 flatpak install flathub org.flameshot.Flameshot
 echo "Finished: Installing GUI tools with flatpak"
